28dbf56db22bd46b20f03324bcef1adaf3ab7355
SAHIL SHARMA <118820911+sahildando@users.noreply.github.com>
2024-10-12 02:24:08 +0530

    Frontend,Backend link and Proxies and CORS

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+import express from 'express';
+
+
+const app = express();
+
+// app.get('/', (req, res) => {
+//     res.send('Server is ready');
+// });
+
+app.get('/api/jokes', (req, res) => {
+  const jokes = [
+    {
+      id: 1,
+      title: 'A joke',
+      content: 'This is a joke'
+    },
+    {
+      id: 1,
+      title: 'Another joke',
+      content: 'This is another joke'
+    }
+  ];
+  res.send(jokes);
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Serve at http://localhost:${port}`);
+
+  }
+);
